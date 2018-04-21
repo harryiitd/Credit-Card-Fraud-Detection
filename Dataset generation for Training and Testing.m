97d054081f799abb4792dab4893309f5bedbd31a
harryiitd <38590446+harryiitd@users.noreply.github.com>
2018-04-21 23:54:24 +0530

    Add files via upload

---
+++ Dataset generation for Training and Testing.m
@@ -0,0 +1,39 @@
+clc
+clear
+t=0;
+g=1;
+f=0;
+s_g = 6;
+mu_g = 30;
+s_f = 40;
+mu_f = 100;
+n=10000;
+v=zeros(n,3);
+s=g;
+rng(1234);
+i=1;
+
+for i=1:n
+    r = rand;
+if(r>=0 && r<=0.7)
+    s=g;
+    t=exprnd(0.25);
+else
+    s=f;
+    t=exprnd(1/9);
+end
+if(s==g)
+    v(i,1) = t;
+    v(i,2) = g;
+    v(i,3) = randn*s_g + mu_g;
+else
+    v(i,1) = t;
+    v(i,2) = f;
+    v(i,3) = randn*s_f + mu_f;
+end
+
+end
+sum(v)
+
+xlswrite('Sim_Matrix.xlsx',v);
+xlsread('Sim_Matrix_train.xlsx');
